3e26b1c5e416157b8777a099f070624b4bb2d6fa
Ville Räisänen <vsr83a@gmail.com>
2024-06-20 01:49:39 +0300

    Progress commit

--- src/Frames.ts
+++ src/Frames.ts
@@ -3,6 +3,7 @@ import { StateVector } from './StateVector';
 import { Rotations } from './Rotations';
 import { NutationData } from './Nutation';
 import { MathUtils } from './MathUtils';
+import { EarthPosition, Wgs84 } from './Wgs84';
 
 /**
  * Enumeration of supported frame orientations.
@@ -25,14 +26,63 @@ export enum FrameOrientation {
  * Enumeration of supported frame centers.
  */
 export enum FrameCenter {
+    // Heliocenter
     CENTER_HELIO,
+    // Solar System Barycenter.
     CENTER_SSB, 
+    // Geocenter.
     CENTER_GEO,
+    // Earth-Moon Barycenter.
     CENTER_EMB,
+    // Topocentric.
     CENTER_TOPOC
 }
 
+/**
+ * 
+ */
 export class FrameConversions {
+    /**
+     * Translate OSV from the geocenter to topocentric location.
+     * 
+     * @param {StateVector} osvGeoEfi
+     *      Target OSV in the geocentric EFI frame. 
+     * @param {EarthPosition} earthPos 
+     *      Topocentric Earth position translated to.
+     * @returns {StateVector} Target OSV in the topocentric EFI frame.
+     */
+    static translateGeoTopo(osvGeoEfi : StateVector, earthPos : EarthPosition) : StateVector {
+        const rEfiEarthPos = Wgs84.coordWgs84Efi(earthPos);
+
+        return {
+            frameCenter : FrameCenter.CENTER_TOPOC,
+            frameOrientation : FrameOrientation.ORIENTATION_EFI,
+            position : MathUtils.vecDiff(osvGeoEfi.position, rEfiEarthPos), 
+            velocity : osvGeoEfi.velocity, 
+            timeStamp : osvGeoEfi.timeStamp
+        };
+    }
+
+    /**
+     * Translate OSV from the topocentric to geocentric location.
+     * 
+     * @param {StateVector} osvTopoEfi
+     *      Target OSV in the topocentric EFI frame. 
+     * @param {EarthPosition} earthPos 
+     *      Topocentric Earth position translated to.
+     * @returns {StateVector} Target OSV in the geocentric EFI frame.
+     */
+    static translateTopoGeo(osvTopoEfi : StateVector, earthPos : EarthPosition) : StateVector {
+        const rEfiEarthPos = Wgs84.coordWgs84Efi(earthPos);
+
+        return {
+            frameCenter : FrameCenter.CENTER_TOPOC,
+            frameOrientation : FrameOrientation.ORIENTATION_EFI,
+            position : MathUtils.vecSum(osvTopoEfi.position, rEfiEarthPos), 
+            velocity : osvTopoEfi.velocity, 
+            timeStamp : osvTopoEfi.timeStamp
+        };
+    }
 
     /**
      * Rotate OSV from J2000 to the Mean-of-Date (MoD) frame.
@@ -106,7 +156,7 @@ export class FrameConversions {
 
         return {
             frameCenter : osvMod.frameCenter,
-            frameOrientation : FrameOrientation.ORIENTATION_MOD,
+            frameOrientation : FrameOrientation.ORIENTATION_J2000_EQ,
             position : rJ2000, 
             velocity : vJ2000, 
             timeStamp : osvMod.timeStamp
@@ -114,7 +164,7 @@ export class FrameConversions {
     }
 
     /**
-     * Convert coordinates from Mean-of-Date (MoD) to the True-of-Date (ToD) frame.
+     * Rotate OSV from Mean-of-Date (MoD) to the True-of-Date (ToD) frame.
      * 
      * @param {StateVector} osv
      *      OSV in MoD frame.
@@ -145,7 +195,7 @@ export class FrameConversions {
     }
 
     /**
-     * Convert coordinates from True-of-Date (ToD) to the Mean-of-Date (MoD) frame.
+     * Rotate OSV from True-of-Date (ToD) to the Mean-of-Date (MoD) frame.
      * 
      * @param {StateVector} osv
      *      OSV in ToD frame.
@@ -177,13 +227,13 @@ export class FrameConversions {
 
 
     /**
-     * Convert coordinates from True-of-Date (ToD) to the Pseudo-Earth-Fixed (PEF)
+     * Rotate OSV from True-of-Date (ToD) to the Pseudo-Earth-Fixed (PEF)
      * frame.
      * 
      * @param {OsvFrame} osv
      *      OSV in ToD frame.
-     * @param {NutationData} nutData 
-     *      Nutation data.
+     * @param {number} GAST 
+     *      Greewich Apparent Sidereal Time (GAST) (in degrees).
      * @returns {OsvFrame} OSV in PEF frame.
      */
     static rotateTodPef(osv : StateVector, GAST : number, jtUt1 : number) : StateVector
@@ -204,7 +254,105 @@ export class FrameConversions {
         vPef[1] += dGASTdt * (Math.PI/180.0) 
                  * (-MathUtils.cosd(GAST) * osv.position[0] - MathUtils.sind(GAST) * osv.position[1]);
 
-        return {frame : Frame.FRAME_PEF,
-            position : rPef, velocity : vPef, timeStamp : osv.timeStamp};
+        return {
+            frameCenter : osv.frameCenter,
+            frameOrientation : FrameOrientation.ORIENTATION_PEF,
+            position : rPef, 
+            velocity : vPef, 
+            timeStamp : osv.timeStamp
+        };
+    }
+
+    /**
+     * Rotate OSV from Pseudo-Earth-Fixed (PEF) to the True-of-Date (ToD)
+     * frame.
+     * 
+     * @param {OsvFrame} osv
+     *      OSV in PEF frame.
+     * @param {number} GAST 
+     *      Greewich Apparent Sidereal Time (GAST) (in degrees).
+     * @returns {OsvFrame} OSV in ToD frame.
+     */
+    static rotatePefTod(osv : StateVector, GAST : number, jtUt1 : number) : StateVector
+    {
+        const rTod = Rotations.rotateCart3d(osv.position, -GAST);
+
+        // Alternative expression for the GMST is \sum_{i=0}^3 k_i MJD^i.
+        const k1 = 360.985647366;
+        const k2 = 2.90788e-13;
+        const k3 = -5.3016e-22;
+        const MJD = jtUt1 - 2451544.5;
+        
+        // Compute time-derivative of the GAST to convert velocities:     
+        const dGASTdt = (1/86400.0) * (k1 + 2*k2*MJD + 3*k3*MJD*MJD);
+
+        let dRdt_rTod = [0, 0, 0];
+        dRdt_rTod[0] = dGASTdt * (Math.PI/180.0) 
+                     * (-MathUtils.sind(GAST) * rTod[0] + MathUtils.cosd(GAST) * rTod[1]); 
+        dRdt_rTod[1] = dGASTdt * (Math.PI/180.0) 
+                     * (-MathUtils.cosd(GAST) * rTod[0] - MathUtils.sind(GAST) * rTod[1]); 
+
+        const vTod = Rotations.rotateCart3d([osv.velocity[0] - dRdt_rTod[0], 
+                                osv.velocity[1] - dRdt_rTod[1], 
+                                osv.velocity[2]], -GAST);
+        return {
+            frameCenter : osv.frameCenter,
+            frameOrientation : FrameOrientation.ORIENTATION_TOD,
+            position : rTod, 
+            velocity : vTod, 
+            timeStamp : osv.timeStamp
+        };
+    }
+
+    /**
+     * Rotate OSV from PEF to the Earth-Fixed (EFI) frame.
+     * 
+     * @param {StateVector} osv 
+     *      OSV in PEF frame.
+     * @param {number} dx 
+     *      Polar motion parameter (in degrees).
+     * @param {number} dy 
+     *      Polar motion parameter (in degrees).
+     * @returns {StateVector} OSV in EFI frame.
+     */
+    static coordPefEfi(osv : StateVector, dx : number, dy : number) : StateVector {
+        const rEfi = Rotations.rotateCart2d(Rotations.rotateCart1d(
+            osv.position, -dy), -dx);
+        const vEfi = Rotations.rotateCart2d(Rotations.rotateCart1d(
+            osv.velocity, -dy), -dx);
+
+        return {
+            frameCenter : osv.frameCenter,
+            frameOrientation : FrameOrientation.ORIENTATION_EFI,
+            position : rEfi, 
+            velocity : vEfi, 
+            timeStamp : osv.timeStamp
+        };
+    }
+
+    /**
+     * Rotate OSV from the Earth-Fixed (EFI) to the PEF frame.
+     * 
+     * @param {StateVector} osv 
+     *      OSV in PEF frame.
+     * @param {number} dx 
+     *      Polar motion parameter (in degrees).
+     * @param {number} dy 
+     *      Polar motion parameter (in degrees).
+     * @returns {StateVector} OSV in PEF frame.
+     */
+    static coordEfiPef(osv : StateVector, dx : number, dy : number) : StateVector {
+        const rPef = Rotations.rotateCart1d(Rotations.rotateCart2d(
+            osv.position, dx), dy);
+        const vPef = Rotations.rotateCart1d(Rotations.rotateCart2d(
+            osv.velocity, dx), dy);
+
+        return {
+            frameCenter : osv.frameCenter,
+            frameOrientation : FrameOrientation.ORIENTATION_PEF,
+            position : rPef, 
+            velocity : vPef, 
+            timeStamp : osv.timeStamp
+        };
     }
 };
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import {TimeCorrelation} from './TimeCorrelation';
 import {Angles} from './Angles';
 import {MathUtils} from './MathUtils';
 import {StateVector} from './StateVector';
+import { Rotations } from './Rotations';
+import { FrameConversions } from './Frames';
 
-export {Angles, MathUtils, StateVector, TimeStamp, TimeCorrelation};
+export {Angles, MathUtils, StateVector, TimeStamp, TimeCorrelation, Rotations, FrameConversions};
